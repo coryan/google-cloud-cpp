f976f98103530dffe2322a8d5c4b6571525804d1
Carlos O'Ryan <coryan@google.com>
2024-07-17 18:19:43 +0000

    WIP - try to repro b/353688666

---
+++ google/cloud/storage/tests/repro_b353688666_test.cc
@@ -0,0 +1,92 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/storage/grpc_plugin.h"
+#include "google/cloud/storage/testing/storage_integration_test.h"
+#include "google/cloud/internal/getenv.h"
+#include "google/cloud/internal/random.h"
+#include "google/cloud/internal/absl_str_cat_quiet.h"
+#include "google/cloud/testing_util/scoped_environment.h"
+#include "google/cloud/testing_util/status_matchers.h"
+#include "absl/strings/string_view.h"
+#include <gmock/gmock.h>
+#include <vector>
+
+namespace google {
+namespace cloud {
+namespace storage {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+namespace {
+
+using ::google::cloud::internal::GetEnv;
+using ::google::cloud::testing_util::ScopedEnvironment;
+using ::testing::Eq;
+using ::testing::IsEmpty;
+using ::testing::Not;
+
+class ObjectRepro353688666IntegrationTest
+    : public google::cloud::storage::testing::StorageIntegrationTest {};
+
+TEST_F(ObjectRepro353688666IntegrationTest, ReadManyRanges) {
+  auto const bucket_name =
+      GetEnv("GOOGLE_CLOUD_CPP_STORAGE_TEST_BUCKET_NAME").value_or("");
+  ASSERT_THAT(bucket_name, Not(IsEmpty()))
+      << "GOOGLE_CLOUD_CPP_STORAGE_TEST_BUCKET_NAME is not set";
+
+  auto client = storage_experimental::DefaultGrpcClient(MakeTestOptions());
+  auto const object_name = MakeRandomObjectName();
+  auto constexpr kMaxRangeSize = 4 * 1024 * 1024;
+  auto constexpr kSize = 8 * kMaxRangeSize;
+  auto const data = MakeRandomData(kSize);
+  auto const view = absl::string_view(data);
+
+  auto insert =
+      client.InsertObject(bucket_name, object_name, data, IfGenerationMatch(0));
+  ASSERT_STATUS_OK(insert);
+  ScheduleForDelete(*insert);
+
+  auto generator = google::cloud::internal::MakeDefaultPRNG();
+  auto start_generator =
+      std::uniform_int_distribution<std::int64_t>(0, kSize - kMaxRangeSize);
+  auto size_generator =
+      std::uniform_int_distribution<std::int64_t>(0, kMaxRangeSize - 1);
+  for (int i = 0; i != 1000; ++i) {
+    if (i != 0 && i % 100 == 0) std::cerr << "iteration=" << i << std::endl;
+    auto const begin = start_generator(generator);
+    auto const size =  size_generator(generator);
+    SCOPED_TRACE(absl::StrCat("Running iteration ", i, " size=", size, " begin=", begin));
+    std::string received;
+    std::vector<char> buffer(128 * 1024);
+    auto is =
+        client.ReadObject(bucket_name, object_name, ReadRange(begin, begin + size));
+    while (!is.eof()) {
+      is.read(buffer.data(), buffer.size());
+      if (is.gcount() == 0) {
+        ASSERT_TRUE(is.eof());
+        continue;
+      }
+      received.append(buffer.begin(), std::next(buffer.begin(), is.gcount()));
+    }
+    EXPECT_FALSE(is.bad());
+    EXPECT_STATUS_OK(is.status());
+    ASSERT_THAT(size, Eq(received.size()));
+    EXPECT_THAT(view.substr(begin, size), Eq(received));
+  }
+}
+
+}  // namespace
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace storage
+}  // namespace cloud
+}  // namespace google
